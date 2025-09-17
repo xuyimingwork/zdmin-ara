01dbf697d2a4f911d8f2172065358f5f32c45d06
xuyimingwork <xuyimingwork@foxmail.com>
2025-09-17 11:56:15 +0800

    feat: setup vite unplugin

--- packages/unplugin/src/index.ts
+++ packages/unplugin/src/index.ts
@@ -1,13 +1,27 @@
 import type { UnpluginFactory, UnpluginInstance } from 'unplugin'
 import { createUnplugin } from 'unplugin'
+import { main } from '@zdmin/ara-local-server'
 
 export interface Options {
   // define your plugin options here
 }
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = (options?: Options) => ({
-  name: 'unplugin-zdmin-ara',
-})
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (options?: Options) => {
+  console.log('hello unplugin')
+  return {
+    name: 'unplugin-zdmin-ara',
+    vite: {
+      configureServer() {
+        main(options).then(({ port }: any) => {
+          console.log('app is running at', port)
+        })
+      }
+    },
+    webpack() {
+
+    }
+  }
+}
 
 export const unplugin: UnpluginInstance<Options | undefined> = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
