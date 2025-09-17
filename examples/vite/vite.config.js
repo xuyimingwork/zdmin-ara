71afd6fcb7427037960ada442b126e22b8432a51
xuyimingwork <xuyimingwork@foxmail.com>
2025-09-17 11:51:22 +0800

    feat: example add docs

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import OpenAPI from '@zdmin/ara-unplugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    OpenAPI({
+      docs: {
+        v2: 'https://petstore.swagger.io/',
+        v3: 'https://petstore3.swagger.io/'
+      }
+    })
+    
   ],
   resolve: {
     alias: {
