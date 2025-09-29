59f47fa19ebceb0f0bc652a7b62a21134cff34fd
xuyimingwork <xuyimingwork@foxmail.com>
2025-09-29 15:11:15 +0800

    feat: update manifest version

--- packages/chrome-extension/vite.config.ts
+++ packages/chrome-extension/vite.config.ts
@@ -7,47 +7,62 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import tailwindcss from '@tailwindcss/vite'
+import { version } from './package.json'
+import { readFile, writeFile } from 'node:fs/promises'
+
+function change(pkg: string) {
+  return readFile(pkg, 'utf-8')
+    .then(content => {
+      return writeFile(pkg, JSON.stringify({
+        ...JSON.parse(content),
+        version
+      }, undefined, 2), 'utf-8')
+    })
+}
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./pages/devtools/openapi-codegen-main.html', import.meta.url)),
-        panel: fileURLToPath(new URL('./pages/devtools/openapi-codegen-panel.html', import.meta.url))
+export default defineConfig(async () => {
+  await change(fileURLToPath(new URL('./public/manifest.json', import.meta.url)))
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          main: fileURLToPath(new URL('./pages/devtools/openapi-codegen-main.html', import.meta.url)),
+          panel: fileURLToPath(new URL('./pages/devtools/openapi-codegen-panel.html', import.meta.url))
+        }
       }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
         @use "@/styles/scss/additional/global.scss" as *;
         `,
+        },
       },
     },
-  },
-  plugins: [
-    vue(), 
-    vueJsx(),
-    tailwindcss(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      resolvers: [
-        ElementPlusResolver({ importStyle: 'sass' })
-      ],
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver({ importStyle: 'sass' }),
-        (name) =>  name === 'Icon' ? { name: 'Icon', from: '@iconify/vue' } : undefined,
-      ],
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '#': fileURLToPath(new URL('./', import.meta.url)),
+    plugins: [
+      vue(),
+      vueJsx(),
+      tailwindcss(),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        resolvers: [
+          ElementPlusResolver({ importStyle: 'sass' })
+        ],
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver({ importStyle: 'sass' }),
+          (name) => name === 'Icon' ? { name: 'Icon', from: '@iconify/vue' } : undefined,
+        ],
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '#': fileURLToPath(new URL('./', import.meta.url)),
+      },
     },
-  },
+  }
 })
