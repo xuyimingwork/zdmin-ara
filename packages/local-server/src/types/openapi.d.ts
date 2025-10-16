a8395a805c31c07b69540a0f1ad222da0252ddc7
xuyimingwork <xuyimingwork@foxmail.com>
2025-10-16 11:00:35 +0800

    feat: type support v3

--- packages/local-server/src/types/openapi.d.ts
+++ packages/local-server/src/types/openapi.d.ts
@@ -8,21 +8,28 @@ import type {
   PathItemObject as OpenAPIPathItemObject3,
   OperationObject as OpenAPIOperationObject3,
 } from "openapi-typescript7";
-import type { ConditionalKeys, Get, SetOptional, IsEqual } from "type-fest";
+import type { ConditionalKeys, Get, SetOptional, IsEqual, ValueOf } from "type-fest";
 
 export type OpenAPI = OpenAPI2 | OpenAPI3
 export type OpenAPIPathOperationObject = OpenAPIOperationObject2 | OpenAPIOperationObject3
 
 export type { 
   OpenAPI2, 
-  OpenAPI3,
+  OpenAPI2,
 }
 
-export type GetResponse<paths, path extends string, method extends string> = Get<Get<paths, `${path}.${method}.responses`>['200'], 'schema'>
+type HasRequestBody<paths, path extends string, method extends string> = Get<paths, `${path}.${method}`> extends { requestBody: any } ? true : false
+type HasResponseContent<paths, path extends string, method extends string> = Get<paths, `${path}.${method}.responses`>['200'] extends { content: any } ? true : false
+
 export type GetRequestParams<paths, path extends string, method extends string> = Get<paths, `${path}.${method}.parameters.path`>
 export type GetRequestQuery<paths, path extends string, method extends string> = Get<paths, `${path}.${method}.parameters.query`>
-// TODO: 兼容 v2(formData) & v3(requestBody)
-export type GetRequestBody<paths, path extends string, method extends string> = Get<paths, `${path}.${method}.parameters.body.body`>
+export type GetRequestBody<paths, path extends string, method extends string> = HasRequestBody<paths, path, method> extends true
+  ? ValueOf<Get<paths, `${path}.${method}.requestBody.content`>>
+  : Get<paths, `${path}.${method}.parameters.body.body`>
+export type GetResponse<paths, path extends string, method extends string> = HasResponseContent<paths, path, method> extends true
+  ? ValueOf<Get<Get<paths, `${path}.${method}.responses`>['200'], 'content'>>
+  : Get<Get<paths, `${path}.${method}.responses`>['200'], 'schema'>
+
 type GetRequestOptionsRaw<paths, path extends string, method extends string> = {
   params: GetRequestParams<paths, path, method>
   query: GetRequestQuery<paths, path, method>
