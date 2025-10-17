c2937c7050702cb7553d3f615221908ba168b8e2
xuyimingwork <xuyimingwork@foxmail.com>
2025-10-17 14:18:29 +0800

    feat(local-server): buildin get-port

--- packages/local-server/tsdown.config.ts
+++ packages/local-server/tsdown.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   exports: true,
   shims: true,
   external: ['typescript'],
+  // buildin get-port to support node14
+  noExternal: ['get-port'],
   format: ['cjs', 'es'],
   target: ['node14'],
   alias: {
