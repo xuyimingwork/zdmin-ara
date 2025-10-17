cf156f7eeb4341749d780d24c8cc28e71193568d
xuyimingwork <xuyimingwork@foxmail.com>
2025-10-17 14:01:53 +0800

    feat(local-server): support node14

--- packages/local-server/tsdown.config.ts
+++ packages/local-server/tsdown.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: 'src/app.ts',
+  exports: true,
+  shims: true,
   external: ['typescript'],
+  format: ['cjs', 'es'],
+  target: ['node14'],
   alias: {
     '@': './src',
     '~': '.',
